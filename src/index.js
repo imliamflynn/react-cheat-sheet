a6dbe4aa25bbd4d2af2779230ca298e4153b6682
Liam Lennon-Flynn <82933053+imliamflynn@users.noreply.github.com>
2025-02-27 17:39:30 +1100

    Added some functions using props and conditionals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello World!</h1>
+function Garage(props) {
+  const cars = props.cars;
+  return (
+    <>
+      <h1>Garage</h1>
+      {cars.length > 0 &&
+      <>
+        <h2>
+          You have {cars.length} cars in your garage.
+        </h2>
+        <p>
+          {cars}
+        </p>
+        </>
+      }
+    </>
+    
+  );
+}
 
+function MissedGoal() {
+  return <h1>MISSED!</h1>;
+}
+
+function MadeGoal() {
+  return <h1>GOAL!</h1>;
+}
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+  return (
+    <>
+      { isGoal ? <MadeGoal/> : <MissedGoal/> }
+    </>
+  );
+}
+
+const cars = ['Ford', 'BMW', 'Audi'];
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(<Garage cars={cars} />);
+
+const root2 = ReactDOM.createRoot(document.getElementById('root2'));
+root2.render(<Goal isGoal={true} />);
