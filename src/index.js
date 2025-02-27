f186b059693368639fe1f2dcee41c834e6f36329
Liam Lennon-Flynn <82933053+imliamflynn@users.noreply.github.com>
2025-02-28 10:11:57 +1100

    Adding array with keys. Turning into list via map function.

--- src/index.js
+++ src/index.js
@@ -38,9 +38,32 @@ function Goal(props) {
   );
 }
 
+function Car(props) {
+  return <li>I am a { props.brand }</li>;
+}
+
+function Garage2() {
+  const cars = [
+    {id: 1, brand: 'Ford'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
+  return (
+    <>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+      </ul>
+    </>
+  );
+}
+
 const cars = ['Ford', 'BMW', 'Audi'];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Garage cars={cars} />);
 
 const root2 = ReactDOM.createRoot(document.getElementById('root2'));
 root2.render(<Goal isGoal={true} />);
+
+const root3 = ReactDOM.createRoot(document.getElementById('root3'));
+root3.render(<Garage2 />);
