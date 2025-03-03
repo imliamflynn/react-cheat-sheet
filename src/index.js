78221b91c208aed4dcc09e9684c8f0ca6b9ec8c9
Liam Lennon-Flynn <82933053+imliamflynn@users.noreply.github.com>
2025-03-04 10:58:02 +1100

    Added router and memo.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { name, age } from "./person.js"; // Named exports must be destructured u
 import message from "./message.js"; // Default exports do not.
 import Comp from "./Comp.js"; // Component in seperate file.
 import { useState } from 'react';
+import { memo } from "react";
 
 // -------------------------------------------------------------------------------------------------------------
 // VARIABLES / JSX
@@ -447,8 +448,170 @@ function myVehicle2({ model, registration: { state } }) {
 // -------------------------------------------------------------------------------------------------------------
 // ROUTER
 // -------------------------------------------------------------------------------------------------------------
+// Create React App doesn't include page routing. React Router is the most popular solution.
+// To add React Router run this in the terminal from the root directory of the application.
+// npm i -D react-router-dom
+
+/*
+To create an application with multiple page routes, let's first start with the file structure.
+Within the src folder, we'll create a folder named pages with several files:
+
+src\pages\:
+  Layout.js
+  Home.js
+  Blogs.js
+  Contact.js
+  NoPage.js
+Each file will contain a very basic React component.
+
+Now we will use our Router in our index.js file:
+
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+Example Explained:
+We wrap our content first with <BrowserRouter>.
+Then we define our <Routes>. An application can have multiple <Routes>. Our basic example only uses one.
+<Route>s can be nested. The first <Route> has a path of / and renders the Layout component.
+The nested <Route>s inherit and add to the parent route. So the blogs path is combined with the parent and becomes /blogs.
+The Home component route does not have a path but has an index attribute. That specifies this route as the default route for the parent route, which is /.
+Setting the path to * will act as a catch-all for any undefined URLs. This is great for a 404 error page.
+
+Pages / Components:
+The Layout component has <Outlet> and <Link> elements.
+The <Outlet> renders the current route selected.
+<Link> is used to set the URL and keep track of browsing history.
+Anytime we link to an internal path, we will use <Link> instead of <a href="">.
+The "layout route" is a shared component that inserts common content on all pages, such as a navigation menu.
+
+Layout.js:
+
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
 
 
+Home.js:
+
+const Home = () => {
+  return <h1>Home</h1>;
+};
+
+export default Home;
+
+
+NoPage.js:
+
+const NoPage = () => {
+  return <h1>404</h1>;
+};
+
+export default NoPage;
+*/
+
+// -------------------------------------------------------------------------------------------------------------
+// MEMO
+// -------------------------------------------------------------------------------------------------------------
+// Using memo will cause React to skip rendering a component if its props have not changed.
+// This can improve performance.
+
+// Without the memos keyword, the below example re-renders the Todos when the increment
+// button is clicked. With the Todos component export wrapped with memo, Todos only
+// re-renders when the todos that are passed to it through props are updated.
+/*
+index.js:
+
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import Todos from "./Todos";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+Todos.js:
+
+import { memo } from "react";
+
+const Todos = ({ todos }) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+      {todos.map((todo, index) => {
+        return <p key={index}>{todo}</p>;
+      })}
+    </>
+  );
+};
+
+export default memo(Todos); // Previous code: export default Todos;
+*/
 
 // -------------------------------------------------------------------------------------------------------------
 // ARROW FUNCTIONS
