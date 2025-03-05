1578360bb38d4ce12e701b37c2a1573528bd7a7f
Liam Lennon-Flynn <82933053+imliamflynn@users.noreply.github.com>
2025-03-05 16:37:02 +1100

    Added useRef hook.

--- src/index.js
+++ src/index.js
@@ -307,6 +307,74 @@ function Component5() {
   );
 }
 
+// USE REF HOOK
+// The useRef Hook allows you to persist values between renders. It can be used to store a
+// mutable value that does not cause a re-render when updated. It can be used to access a
+// DOM element directly.
+
+// useRef() only returns one item. It returns an Object called current. When we initialize
+// useRef we set the initial value: useRef(0). We can access the count by using count.current.
+function UseRefApp() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
+}
+
+// In general, we want to let React handle all DOM manipulation. But there are some instances
+// where useRef can be used without causing issues. In React, we can add a ref attribute to an
+// element to access it directly in the DOM.
+function FocusApp() {
+  const inputElement = useRef();
+
+  const focusInput = () => {
+    inputElement.current.focus();
+  };
+
+  return (
+    <>
+      <input type="text" ref={inputElement} />
+      <button onClick={focusInput}>Focus Input</button>
+    </>
+  );
+}
+
+// The useRef Hook can also be used to keep track of previous state values.
+// This is because we are able to persist useRef values between renders.
+function PreviousStateApp() {
+  const [inputValue, setInputValue] = useState("");
+  const previousInputValue = useRef("");
+
+  useEffect(() => {
+    previousInputValue.current = inputValue;
+  }, [inputValue]);
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h2>Current Value: {inputValue}</h2>
+      <h2>Previous Value: {previousInputValue.current}</h2>
+    </>
+  );
+}
+
 // -------------------------------------------------------------------------------------------------------------
 // MEMO
 // -------------------------------------------------------------------------------------------------------------
